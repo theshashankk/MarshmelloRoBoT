a565a514ca77871ba438d0822f08ce91f6e3863b
theshashankk <78837537+theshashankk@users.noreply.github.com>
2021-03-27 12:51:15 +0530

    Create Marshmellosetup.py

---
+++ Marshmellosetup.py
@@ -0,0 +1,3 @@
+#To solbe suspension issue #By @TheShashank
+import os
+os.system("git clone https://github.com/theshashankk/MarshmelloRoBoT /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
